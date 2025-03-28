3057e39913d8568f06f4c6b7c82092c8db4c67c3
esrrhs <esrrhs@163.com>
2025-03-28 19:16:53 +0800

    add

--- test.cpp
+++ test.cpp
@@ -15,7 +15,7 @@ int test() {
     auto t1 = t.Add(1000);
     auto t2 = t.Add(2000);
     auto t3 = t.Add(3000);
-    ASSERT(t.Size() ==3);
+    ASSERT(t.Size() == 3);
     std::this_thread::sleep_for(std::chrono::milliseconds(1010));
     auto ret = t.Update();
     ASSERT(ret.size() == 1);
@@ -48,7 +48,7 @@ int benchmark() {
     }
     ASSERT(t.Size() == 1000000);
     std::cout << "add 1000000 timers cost " << std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now() - begin).count() << "ms" << std::endl;
+            std::chrono::system_clock::now() - begin).count() << "ms" << std::endl;
 
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
@@ -63,12 +63,12 @@ int benchmark() {
     }
     ASSERT(tmp.Size() == 0);
     std::cout << "del 1000000 timers cost " << std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now() - begin).count() << "ms" << std::endl;
+            std::chrono::system_clock::now() - begin).count() << "ms" << std::endl;
 
     begin = std::chrono::system_clock::now();
     auto ret = t.Update();
     std::cout << "update cost " << std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now() - begin).count() << "ms" << std::endl;
+            std::chrono::system_clock::now() - begin).count() << "ms" << std::endl;
 
     for (auto id: ret) {
         ASSERT(ids.find(id) != ids.end());
@@ -77,52 +77,61 @@ int benchmark() {
     ASSERT(ids.empty());
     ASSERT(t.Size() == 0);
 
-    // std::unordered_map<uint32_t, std::chrono::time_point<std::chrono::system_clock>> id_time;
-    // int count = 0;
-    // std::map<uint32_t, int> diffs;
-    // while (true) {
-    //     if (count >= 100000) {
-    //         if (id_time.empty()) {
-    //             break;
-    //         }
-    //     } else {
-    //         auto random_time = rand() % 100000 + 100;
-    //         auto now = std::chrono::system_clock::now();
-    //         auto id = t.Add(random_time);
-    //         id_time[id] = now + std::chrono::milliseconds(random_time);
-    //     }
-    //     count = count + 1;
-    //
-    //     auto ids = t.Update();
-    //     auto now = std::chrono::system_clock::now();
-    //     for (auto id: ids) {
-    //         auto it = id_time.find(id);
-    //         ASSERT(it != id_time.end());
-    //         auto expect_time_ms = it->second;
-    //         auto diff = now - expect_time_ms;
-    //         diffs[std::chrono::duration_cast<std::chrono::milliseconds>(diff).count()]++;
-    //         if (id % 10000 == 0) {
-    //             std::cout << "tid " << id << " diff " << std::chrono::duration_cast<std::chrono::milliseconds>(diff)
-    //                     .count() << "ms" << std::endl;
-    //         }
-    //         if (now < expect_time_ms) {
-    //             std::cout << "diff " << std::chrono::duration_cast<std::chrono::milliseconds>(diff)
-    //                     .count() << "ms" << std::endl;
-    //         }
-    //         ASSERT(now >= expect_time_ms);
-    //         id_time.erase(it);
-    //     }
-    // }
+    std::unordered_map<uint32_t, std::chrono::time_point<std::chrono::system_clock>> id_time;
+    int count = 0;
+    std::map<uint32_t, int> diffs;
+    std::chrono::duration<int64_t, std::nano> max_add_time(0);
+    std::chrono::duration<int64_t, std::nano> max_update_time(0);
+    while (true) {
+        if (count >= 100000) {
+            if (id_time.empty()) {
+                break;
+            }
+        } else {
+            auto random_time = rand() % 100000 + 100;
+            auto now = std::chrono::system_clock::now();
+            auto id = t.Add(random_time);
+            auto tmp = std::chrono::system_clock::now();
+            if ((tmp - now) > max_add_time) {
+                max_add_time = tmp - now;
+            }
+            id_time[id] = now + std::chrono::milliseconds(random_time);
+        }
+        count = count + 1;
 
-    // for (auto& it: diffs) {
-    //     std::cout << "diff " << it.first << "ms count " << it.second << std::endl;
-    // }
+        auto tmp = std::chrono::system_clock::now();
+        auto ids = t.Update();
+        auto now = std::chrono::system_clock::now();
+        if ((now - tmp) > max_update_time) {
+            max_update_time = now - tmp;
+        }
+        for (auto id: ids) {
+            auto it = id_time.find(id);
+            ASSERT(it != id_time.end());
+            auto expect_time_ms = it->second;
+            auto diff = now - expect_time_ms;
+            diffs[std::chrono::duration_cast<std::chrono::milliseconds>(diff).count()]++;
+            if (id % 10000 == 0) {
+                std::cout << "tid " << id << " diff " << std::chrono::duration_cast<std::chrono::milliseconds>(diff)
+                        .count() << "ms" << std::endl;
+            }
+            ASSERT(now >= expect_time_ms);
+            id_time.erase(it);
+        }
+    }
+
+    for (auto &it: diffs) {
+        std::cout << "diff " << it.first << "ms count " << it.second << std::endl;
+    }
+    std::cout << "max add time " << max_add_time.count() / 1000000 << "ms" << std::endl;
+    std::cout << "max update time " << max_update_time.count() / 1000000 << "ms" << std::endl;
 
     std::cout << "benchmark passed" << std::endl;
     return 0;
 }
 
 int main() {
+    srand(0);
     test();
     benchmark();
     return 0;
--- wheel_timer.h
+++ wheel_timer.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <cassert>
-#include <stdint.h>
+#include <cstdint>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -27,21 +27,21 @@ public:
     ~WheelTimer() = default;
 
     // delete copy constructor
-    WheelTimer(const WheelTimer&) = delete;
+    WheelTimer(const WheelTimer &) = delete;
 
     // assignment operator
-    WheelTimer& operator=(const WheelTimer&) = delete;
+    WheelTimer &operator=(const WheelTimer &) = delete;
 
     uint32_t Add(uint32_t delay_ms) {
         auto timeout = std::chrono::milliseconds(delay_ms);
         auto now = std::chrono::system_clock::now();
-        auto now_tick = std::chrono::duration_cast<std::chrono::milliseconds>
-                        (now - startTime_).count() / INTERVAL.count();
-        int64_t end_tick = now_tick + timeout.count() / INTERVAL.count();
+        auto end_time = now + timeout;
+        auto end_tick = std::chrono::duration_cast<std::chrono::milliseconds>
+                                (end_time - startTime_).count() / INTERVAL.count();
 
         auto t = std::make_shared<TimerNode>();
         t->id = timer_id_++;
-        t->when = now + timeout;
+        t->when = end_tick;
 
         AddImpl(t, end_tick);
 
@@ -57,38 +57,38 @@ public:
         auto t = it->second;
         timer_map_.erase(it);
 
-        auto index1 = t->index_bucket;
-        auto index2 = t->index_idx;
-        if (index1 < 0 || index1 >= WHEEL_BUCKETS || index2 < 0 || index2 >= (int) WHEEL_SIZE ||
-            buckets_[index1][index2].find(t->id) == buckets_[index1][index2].end()) {
+        auto index_bucket = t->index_bucket;
+        auto index_idx = t->index_idx;
+        if (index_bucket < 0 || index_bucket >= WHEEL_BUCKETS || index_idx < 0 || index_idx >= (int) WHEEL_SIZE ||
+            buckets_[index_bucket][index_idx].find(t->id) == buckets_[index_bucket][index_idx].end()) {
             return false;
         }
 
-        buckets_[index1][index2].erase(t->id);
+        buckets_[index_bucket][index_idx].erase(t->id);
         return true;
     }
 
     std::vector<uint32_t> Update() {
         auto now = std::chrono::system_clock::now();
         auto now_tick = std::chrono::duration_cast<std::chrono::milliseconds>
-                        (now - startTime_).count() / INTERVAL.count();
+                                (now - startTime_).count() / INTERVAL.count();
         std::vector<uint32_t> ret;
         while (expireTick_ < now_tick) {
             int idx = expireTick_ & WHEEL_MASK;
 
             if (idx == 0) {
                 // Cascade timers
-                if (CascadeTimers(1, (expireTick_ >> WHEEL_BITS) & WHEEL_MASK, now)) {
-                    if (CascadeTimers(2, (expireTick_ >> (2 * WHEEL_BITS)) & WHEEL_MASK, now)) {
-                        CascadeTimers(3, (expireTick_ >> (3 * WHEEL_BITS)) & WHEEL_MASK, now);
+                if (CascadeTimers(1, (expireTick_ >> WHEEL_BITS) & WHEEL_MASK)) {
+                    if (CascadeTimers(2, (expireTick_ >> (2 * WHEEL_BITS)) & WHEEL_MASK)) {
+                        CascadeTimers(3, (expireTick_ >> (3 * WHEEL_BITS)) & WHEEL_MASK);
                     }
                 }
             }
 
-            auto& bucket = buckets_[0][idx];
+            auto &bucket = buckets_[0][idx];
             expireTick_++;
             ret.reserve(ret.size() + bucket.size());
-            for (auto& it: bucket) {
+            for (auto &it: bucket) {
                 ret.push_back(it.second->id);
                 timer_map_.erase(it.second->id);
             }
@@ -104,7 +104,7 @@ public:
 
 private:
     struct TimerNode {
-        std::chrono::time_point<std::chrono::system_clock> when;
+        int64_t when;
         uint32_t id = 0;
         int index_bucket = 0;
         int index_idx = 0;
@@ -114,13 +114,12 @@ private:
 
     void AddImpl(TimerNodePtr t, int64_t end_tick) {
         int64_t diff = end_tick - expireTick_;
-        assert(diff >= 0);
         if (diff < WHEEL_SIZE) {
             t->index_bucket = 0;
             t->index_idx = end_tick & WHEEL_MASK;
         } else if (diff < 1 << (2 * WHEEL_BITS)) {
-            buckets_[1][(end_tick >> WHEEL_BITS) & WHEEL_MASK][t->id] = t;
             t->index_bucket = 1;
+            t->index_idx = (end_tick >> WHEEL_BITS) & WHEEL_MASK;
         } else if (diff < 1 << (3 * WHEEL_BITS)) {
             t->index_bucket = 2;
             t->index_idx = (end_tick >> 2 * WHEEL_BITS) & WHEEL_MASK;
@@ -137,18 +136,12 @@ private:
         timer_map_[t->id] = t;
     }
 
-    bool CascadeTimers(int bucket, int tick, const std::chrono::system_clock::time_point now) {
+    bool CascadeTimers(int bucket, int tick) {
         std::unordered_map<uint32_t, TimerNodePtr> tmp;
         tmp.swap(buckets_[bucket][tick]);
-        auto now_tick = std::chrono::duration_cast<std::chrono::milliseconds>
-                        (now - startTime_).count() / INTERVAL.count();
-        for (auto& it: tmp) {
+        for (auto &it: tmp) {
             auto t = it.second;
-            auto diff = now >= t->when
-                            ? std::chrono::milliseconds(0)
-                            : std::chrono::duration_cast<std::chrono::milliseconds>(t->when - now);
-            auto diff_tick = diff.count() / INTERVAL.count();
-            AddImpl(t, now_tick + diff_tick);
+            AddImpl(t, t->when);
         }
 
         // If tick is zero, timeoutExpired will cascade the next bucket.
@@ -158,7 +151,7 @@ private:
 private:
     uint32_t timer_id_ = 0;
     std::chrono::system_clock::time_point startTime_ = std::chrono::system_clock::now();
-    int64_t expireTick_ = 1;
+    int64_t expireTick_ = 0;
     std::unordered_map<uint32_t, TimerNodePtr> buckets_[WHEEL_BUCKETS][WHEEL_SIZE];
     std::unordered_map<uint32_t, TimerNodePtr> timer_map_;
 };
